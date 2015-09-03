01e259e04be74cda67038d8f1e5ce1f835433b8d
Bob Amand <biotmg@nc.rr.com>
2015-09-02 23:10:28 -0400

    Not handling punctuation

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,31 @@
+# Palindrome detector: Reads forward or backward.
+
+sentence = input("Enter a phrase you think is a Palindrome: ")
+
 def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+    low_sent = sentence.lower() # takes the lower case of the sentence
+    cluster = low_sent.split()  # splitting the words into a list
+    fr_A = "".join(cluster)    # creating a list of the letters, all words, without spaces
+    list_sent = (fr_A)
+    fr_B = []                   # empty list to take ONLY the letters (a to z)
+    punct = ('abcdefghijklmnopqrstuvwxyz')  # comparitor list of letters (a to z)
+    for x in list_sent:
+        for i in punct:
+            if punct[i] == list_sent[x]:     #TypeError: list indices must be integers, not str
+                fr_B.append(list_sent[x])
+    ba = fr_B[::-1]
+    if ba == fr_A:
+        return ("{} is a palindrome." .format(sentence))
+    else:
+        return ("{} is not a palindrome." .format(sentence))
 
+print(is_palindrome(sentence))
 
-def main():
-    # TODO: put your input/output code here
-    pass
+'''
+#def main():
+#    # TODO: put your input/output code here
+#    pass
 
-
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
+'''
